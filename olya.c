15a808af5cd34586aa12fb1cd1604f86476f0897
NittaDragon <uspemskaya@gmail.com>
2022-04-04 11:36:13 +0300

    wow

---
+++ olya.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void move (int n, char a, char b, char c)
+{
+  if (n==1)
+  {
+    printf("\t %c -> %c \n",a,b);
+    printf("\t %c -> %c \n",b,c);
+  }
+  else
+    {
+      move(n-1,a,b,c);
+      printf("\t %c -> %c \n",a,b);
+      move(n-1,b,c,a);
+      //printf("\t %c -> %c \n",b,c);
+    }
+}
+int main()
+{
+  int n;
+  printf("количество:");
+  scanf("%d",&n);
+  move(n,'a','b','c');
+}
